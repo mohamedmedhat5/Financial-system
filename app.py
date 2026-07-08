a51ebd4f78b0200dc48da9c33fdf46e3864d5a1c
Mohamed Medhat <mmedhat2205@gmail.com>
2026-07-08 20:42:26 +0300

    Prepare project for Render deployment

--- app.py
+++ app.py
@@ -642,12 +642,10 @@ def admin_logout():
 
 if __name__ == "__main__":
 
+    import os
+
     app.run(
-
-        host="127.0.0.1",
-
-        port=5000,
-
-        debug=True
-
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
     )
